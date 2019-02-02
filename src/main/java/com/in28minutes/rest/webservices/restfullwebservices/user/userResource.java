b6511061ec9977859a488976b954d69d22e68c7d
Purushotam Thakur <purushotam261850@gmail.com>
2019-02-02 23:24:27 +0530

    Exception NOT FOUND added

--- src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/userResource.java
+++ src/main/java/com/in28minutes/rest/webservices/restfullwebservices/user/userResource.java
@@ -24,7 +24,12 @@ public class userResource {
 
     @GetMapping("/users/{id}")
     public User retriveAllUsers(@PathVariable int id){
-        return service.findOne(id);
+        User user =  service.findOne(id);
+
+        if(user==null)
+            throw new UserNotFoundException("id-"+id);
+
+        return user;
     }
 
     @PostMapping("/users")
